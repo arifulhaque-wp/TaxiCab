8283121bfdc8cd6870668038248c4e674b847c8c
arifulhaque_wp <arifulhaque.wp@gmail.com>
2021-03-21 02:25:47 +0600

    mobile menu

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,12 +1,24 @@
-(function ($) {
-  "Use Strict";
+// (function ($) {
+//   "Use Strict";
 
-  $('.main-menu li a').click(function(e) {
-    e.preventDefault();
-    $('a').removeClass('active');
-    $(this).addClass('active');
-  });
+//   $('.main-menu li a').click(function(e) {
+//     e.preventDefault();
+//     $('a').removeClass('active');
+//     $(this).addClass('active');
+//   });
 
-  
+// }) (jQuery);
 
-}) (jQuery);
+
+const menu = document.querySelector(".main-menu ul");
+const menuBars = document.querySelector(".bars");
+const menuClose = document.querySelector(".close-btn");
+
+menuBars.onclick = ()=> {
+  menu.classList.add("active");
+  menuBars.classList.add("hide");
+}
+menuClose.onclick = ()=> {
+  menu.classList.remove("active");
+  menuBars.classList.remove("hide");
+}
